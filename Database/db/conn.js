dd64a3bf9821e28d46cf36e5cc7f431d4e3e9f14
Debojit219 <debojit.banerjee546@gmail.com>
2021-12-26 12:12:49 +0530

    connecting DB with atlas

--- Database/db/conn.js
+++ Database/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/realTimeChatAppDB').then(()=>{
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/realTimeChatAppDB').then(()=>{
     console.log("DB Connection Successful");
 }).catch((e)=>{
     console.log("DB Connection error!!");
