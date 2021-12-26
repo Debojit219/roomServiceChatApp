775c8bd273fcf35e46de2fa3af346fa8e183230a
Debojit219 <debojit.banerjee546@gmail.com>
2021-12-26 12:18:53 +0530

    clientSide port Change

--- public/js/clientScript.js
+++ public/js/clientScript.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000")
+const socket = io(process.env.PORT || "http://localhost:3000")
 
 const messageForm = document.getElementById("send-container")
 const msgInput = document.getElementById("messageInput")
